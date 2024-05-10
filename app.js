b5f6eaacf92fa8ee9a19da2826bb9d153fa7a293
Saniya-Kumari123 <164067091+Saniya-Kumari123@users.noreply.github.com>
2024-05-10 23:50:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+ const bodyParser = require('body-parser');
+ const productRoute = require('./routes/index')
+
+
+// Create an Express application
+const app = express();
+
+// Set up middleware
+ app.use(bodyParser.json());
+  app.use(productRoute)
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/shopping', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+
+ // Handle 404 errors
+  app.use((req, res, next) => {
+  const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+ });
+
+// Error handling middleware
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  });
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
